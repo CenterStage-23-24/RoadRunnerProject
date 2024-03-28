6af8b33eebd39b8ad808d2b2f9e839b050189476
OmkarRobotics <122411633+OmkarRobotics@users.noreply.github.com>
2024-03-28 18:28:52 -0400

    made a trajectory and ran on meep meep. Done with V1

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -13,8 +14,12 @@ public class MeepMeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(30, 30, Math.toRadians(60), Math.toRadians(60), 14)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-
+                        drive.trajectorySequenceBuilder(new Pose2d(17, 64, Math.toRadians(270)))
+                                .lineTo(new Vector2d(40,38))
+                                .turn(Math.toRadians(90))
+                                .lineTo(new Vector2d(48,38))
+                                .lineTo(new Vector2d(12,38))
+                                .turn(Math.toRadians(-90))
                                 .build()
                 );
 
